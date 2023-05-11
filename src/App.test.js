0c1ba30c8a25c318552402611fafdac8c7db3738
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-05-11 15:59:10 +0530

    RideOn1 [Shivangi, Aishwarya] Fix for lint issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 import React from 'react'
-/* eslint-disable */
+
 test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/Welcome to RideOn/i)

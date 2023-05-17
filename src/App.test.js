28cd0fc8928decc7a56b6c15f7c0e18fa8482d37
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-05-17 16:56:30 +0530

    Revert "refactor: test to follow AAA [Shivangi, Khushboo][RideOn12]"
    This reverts commit 1934fe56388f85298ebe1a7d4805cb2ca624649f.

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,5 @@ import React from "react";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to RideOn/i);
-
   expect(linkElement).toBeInTheDocument();
 });

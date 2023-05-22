b3517b69e535ef6555a7ab0718074eb804d2dd39
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-05-22 22:41:33 +0530

    ci: add audit and test in pre-commit [Shivangi][RideOn11]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+import {Button} from "@mui/material";
 
 function App() {
+  const a = "#@4567";
   return (
     <div className="App">
+      <Button variant="contained" color="primary">
+        ${a}
+      </Button>
       <header className="App-header">
         <h2>Welcome to RideOn!</h2>
       </header>

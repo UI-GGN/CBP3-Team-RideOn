4edb479392924020e42da8ac38eef60d14888484
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-05-24 10:53:19 +0530

    ci: add login page [Shivangi][RideOn13]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./App.css";
-import {Button} from "@mui/material";
+import Login from "./pages/login/login";
 
 function App() {
-  const a = "#@4567";
   return (
-    <div className="App">
-      <Button variant="contained" color="primary">
-        ${a}
-      </Button>
-      <header className="App-header">
-        <h2>Welcome to RideOn</h2>
-      </header>
+    <div>
+      <Login />
     </div>
   );
 }

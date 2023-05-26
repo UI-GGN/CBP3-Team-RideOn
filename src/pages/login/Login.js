3c0f4b891c15029a22f4171b787a35363d63e08d
Shivangip285 <106912393+Shivangip285@users.noreply.github.com>
2023-05-26 23:50:50 +0530

    refactor: login page styling  [Shivangi][RideOn13]
    * refactor: login page styling  [Shivangi][RideOn13]
    
    * refactor: change image from png to svg  [Shivangi][RideOn13]
    
    * Update Login.css [Shivangi][RideOn13]
    
    * refactor: pre commit hook for tailsman setup [Shivangi][RideOn13]
    
    * revert: script [Shivangi][RideOn13]

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,16 +1,16 @@
 import React from "react";
 import {Button} from "@mui/material";
-import login from "../../assets/Login.png";
-import logo from "../../assets/Logo.png";
+import loginImage from "../../assets/Login.svg";
+import logoImage from "../../assets/Logo.svg";
 import "./Login.css";
 
 function Login() {
   return (
     <div>
-      <img src={logo} className="logo" />
-      <div className="body">
+      <img src={logoImage} className="logo" />
+      <div className="login-page-body">
         <div>
-          <img src={login} className="body-left" />
+          <img src={loginImage} className="body-left" />
         </div>
         <div className="body-right">
           <div>

1b1dc577ecb052e6337843c012a8f044d4c871da
Shiwani Mishra <mshiwani99@gmail.com>
2023-05-27 18:30:17 +0530

    feat: add a test for login functionality

---
+++ src/pages/login/Login.test.js
@@ -0,0 +1,20 @@
+import {render, screen, fireEvent, waitFor} from "@testing-library/react";
+import Login from "./Login";
+import { useAuth0 } from "@auth0/auth0-react";
+
+jest.mock("@auth0/auth0-react");
+
+describe("Login Page", () => {
+  it("should call loginWithRedirect method on onClick", async () => {
+    useAuth0.mockReturnValue({
+      loginWithRedirect: jest.fn()
+    });
+    const { loginWithRedirect } = useAuth0();
+
+    render(<Login/>);
+    const logInButton = await screen.getByText("LOGIN WITH GOOGLE");
+    fireEvent.click(logInButton);
+
+    await waitFor(() => expect(loginWithRedirect).toHaveBeenCalledTimes(1));
+  });
+});

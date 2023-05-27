0346d8a004aa97c32d7aacdd2dced15eb1a8624a
Shiwani Mishra <mshiwani99@gmail.com>
2023-05-27 14:48:34 +0530

    feat: Add env file to get authO config [Evan/Shiwani]

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,13 +3,13 @@ import {Button} from "@mui/material";
 import "./Dasboard.css";
 
 function Dashboard() {
-  const {logout, user} = useAuth0();
+  const {logout, user, isAuthenticated} = useAuth0();
   return (
     <div className="dashboard">
       <Button variant="contained" size="large" onClick={() => logout()}>
         Logout
       </Button>
-      {console.log(user)}
+      {isAuthenticated && console.log(user)}
       <div className="welcome-message">Welcome to Ride On {user ? user.name : ""}</div>
     </div>
   );

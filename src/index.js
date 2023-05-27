8d8e5e53fe627488336fd483ff7b81cab0e0192f
Shiwani Mishra <mshiwani99@gmail.com>
2023-05-27 15:21:30 +0530

    feat: Add env file to get authO config [Evan/Shiwani] 4

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {Auth0Provider} from "@auth0/auth0-react";
-import Config from "./config/config";
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={Config.AUTH0_DOMAIN}
-      clientId={Config.AUTH0_CLIENT_ID}
-      redirectUri={Config.AUTH0_REDIRECT_URI}
+      domain="dev-11jeq2vs.us.auth0.com"
+      clientId="MDGSSxqOah7fNQVvndBh3abAZlgj41AM"
+      redirectUri="https://rideongurgaon.netlify.app/dashboard"
       scopes="roles"
     >
       <BrowserRouter>

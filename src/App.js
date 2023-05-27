689f64c2ee9a12c428dc5cc5d6f2d0fca708329e
Shiwani Mishra <mshiwani99@gmail.com>
2023-05-27 15:36:39 +0530

    feat: Add env file to get authO config [Evan/Shiwani] 5

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Login/>
     </div>
   );
 }

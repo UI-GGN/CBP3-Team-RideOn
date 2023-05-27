96bc345863fc3925b04a715a690deba674a23fff
Shiwani Mishra <mshiwani99@gmail.com>
2023-05-27 17:56:12 +0530

    fix: app.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import {render, screen} from "@testing-library/react";
 import App from "./App";
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-test.skip("renders learn react link", () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  );
   const linkElement = screen.getByText(/What is RideOn?/i);
   expect(linkElement).toBeInTheDocument();
 });

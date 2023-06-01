a2777823507e436a0e151ccbe783d23e31b66ef1
Pulkit Kogta <pulkit.kogta@thoughtworks.com>
2023-06-01 20:28:16 +0530

    feat: dashboard WIP

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 import {Route, Routes} from "react-router-dom";
+import Dashboard2 from './pages/dashboard2/Dashboard2';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard2" element={<Dashboard2 />} />
       </Routes>
     </div>
   );
---
+++ src/pages/dashboard2/Dashboard2.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PermanentDrawerLeft from '../../component/PermanentDrawerLeft';
+
+class Dashboard2 extends Component {
+  render() {
+    return (
+      <div>
+      {<PermanentDrawerLeft/>}
+      </div>
+    );
+  }
+}
+
+export default Dashboard2;

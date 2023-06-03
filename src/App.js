25745563401b59c4ca955f2852970b748e030fb6
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-06-03 20:25:55 +0530

    ci: display breadcrump on navigation

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
     </div>
   );

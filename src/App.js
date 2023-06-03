80bc2ecf3e9add7054ebd44dcabe61e94ac6a74d
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-06-04 01:17:59 +0530

    feat: add routes and requests component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
     </div>

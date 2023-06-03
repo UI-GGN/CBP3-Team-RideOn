b7db04e12817260c28b33cac75b293861737aee9
Shivangi Pandey <shivangi.pandey@thoughtworks.com>
2023-06-04 01:17:59 +0530

    feat: add routes and requests component

---
+++ src/component/DashboardRequests.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DashboardBreadCrump from "./DashboardBreadCrump";
+
+function DashboardRoutes() {
+  return (
+      <DashboardBreadCrump type="Requests"/>
+  );
+}
+
+export default DashboardRoutes;
--- src/component/PermanentDrawerLeft.js
+++ src/component/PermanentDrawerLeft.js
@@ -15,7 +15,8 @@ import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import logoImage from "../assets/Logo.svg";
 import {useAuth0} from "@auth0/auth0-react";
 import "./PermanentDrawerLeft.css";
-import DashboardBreadCrump from "../pages/login/DashboardBreadCrump";
+import DashboardRoutes from "./DashboardRoutes";
+import DashboardRequests from "./DashboardRequests";
 
 const drawerWidth = 350;
 
@@ -41,102 +42,104 @@ export default function PermanentDrawerLeft() {
   const navigate = useNavigate();
 
   const handleClick = (text) => {
-    console.log("b");
     setSelectedItem(text);
-    navigate('/dashboard/' + text);
+    navigate("/dashboard/" + text);
   };
 
   return (
-      <div>
-    <Box sx={{display: "flex"}}>
-      <Drawer
-        PaperProps={{
-          sx: {
-            backgroundColor: "black",
-            color: "white",
-          },
-        }}
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
+    <div>
+      <Box sx={{display: "flex"}}>
+        <Drawer
+          PaperProps={{
+            sx: {
+              backgroundColor: "black",
+              color: "white",
+            },
+          }}
+          sx={{
             width: drawerWidth,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Box sx={{marginBottom: "130px"}}>
-          <img
-            src={logoImage}
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Box sx={{marginBottom: "130px"}}>
+            <img
+              src={logoImage}
+              style={{
+                width: "100px",
+                height: "48px",
+                marginTop: "40px",
+                marginLeft: "35px",
+              }}
+            />
+          </Box>
+          <Divider sx={{backgroundColor: "white", marginBottom: "40px"}} />
+          <List>
+            <ListItem
+              key={"Routes"}
+              onClick={() => handleClick("Routes")}
+              sx={{
+                backgroundColor: selectedItem === "Routes" ? "#1976D2" : "initial",
+                "&:hover": {
+                  backgroundColor: "#1976D2",
+                },
+              }}
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  <DirectionsOutlinedIcon sx={{color: "white", fontSize: "30px"}} />
+                </ListItemIcon>
+                <ListItemText primary={getTypography("Routes")} />
+              </ListItemButton>
+            </ListItem>
+            <ListItem
+              key={"Requests"}
+              onClick={() => handleClick("Requests")}
+              sx={{
+                backgroundColor: selectedItem === "Requests" ? "#1976D2" : "initial",
+                "&:hover": {
+                  backgroundColor: "#1976D2",
+                },
+              }}
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  <BackHandOutlinedIcon sx={{color: "white", fontSize: "30px"}} />
+                </ListItemIcon>
+                <ListItemText primary={getTypography("Requests")} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <List
             style={{
-              width: "100px",
-              height: "48px",
-              marginTop: "40px",
-              marginLeft: "35px",
-            }}
-          />
-        </Box>
-        <Divider sx={{backgroundColor: "white", marginBottom: "40px"}} />
-        <List>
-          <ListItem
-            key={"Routes"}
-            onClick={() => handleClick("Routes")}
-            sx={{
-              backgroundColor: selectedItem === "Routes" ? "#1976D2" : "initial",
-              "&:hover": {
-                backgroundColor: "#1976D2",
-              },
+              position: "absolute",
+              bottom: "0",
+              left: "0",
+              right: "0",
+              padding: "8px",
             }}
           >
-            <ListItemButton >
-              <ListItemIcon>
-                <DirectionsOutlinedIcon sx={{color: "white", fontSize: "30px"}} />
-              </ListItemIcon>
-              <ListItemText primary={getTypography("Routes")} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem
-            key={"Requests"}
-            onClick={() => handleClick("Requests")}
-            sx={{
-              backgroundColor: selectedItem === "Requests" ? "#1976D2" : "initial",
-              "&:hover": {
-                backgroundColor: "#1976D2",
-              },
-            }}
-          >
-            <ListItemButton >
-              <ListItemIcon>
-                <BackHandOutlinedIcon sx={{color: "white", fontSize: "30px"}} />
-              </ListItemIcon>
-              <ListItemText primary={getTypography("Requests")} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-        <List style={{
-          position: "absolute",
-          bottom: "0",
-          left: "0",
-          right: "0",
-          padding: "8px",
-        }}>
-          <ListItem onClick={() => logout()}>
-            <ListItemButton>
-              <ListItemIcon>
-                <LogoutOutlinedIcon sx={{color: "white", fontSize: "30px"}} />
-              </ListItemIcon>
-              <ListItemText primary={getTypography("Logout")} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Drawer>
-    </Box>
-    <ReactRoutes>
-        <Route path='/' element={<DashboardBreadCrump type="Routes" />} />
-        <Route path='requests' element={<DashboardBreadCrump type="Requests" />} />
-        <Route path='routes' element={<DashboardBreadCrump type="Routes" />} />
-    </ReactRoutes>
-      </div>);
+            <ListItem onClick={() => logout()}>
+              <ListItemButton>
+                <ListItemIcon>
+                  <LogoutOutlinedIcon sx={{color: "white", fontSize: "30px"}} />
+                </ListItemIcon>
+                <ListItemText primary={getTypography("Logout")} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+      </Box>
+      <ReactRoutes>
+        <Route path="/" element={<DashboardRoutes/>} />
+        <Route path="requests" element={<DashboardRequests/>} />
+        <Route path="routes" element={<DashboardRoutes/>} />
+      </ReactRoutes>
+    </div>
+  );
 }

63863dc9a7b5b11a8bda1f08934ae2481bb802cf
Shiwani Mishra <mshiwani99@gmail.com>
2023-06-05 18:22:41 +0530

    refactor: move the dashboard routes to app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 import {Route, Routes} from "react-router-dom";
+import DashboardRoutes from "./component/DashboardRoutes";
+import DashboardRequests from "./component/DashboardRequests";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard/*" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} >
+          <Route index element={<DashboardRoutes />} />
+          <Route path="requests" element={<DashboardRequests />} />
+          <Route path="routes" element={<DashboardRoutes />} />
+        </Route>
       </Routes>
     </div>
   );

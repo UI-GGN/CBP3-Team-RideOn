9d2fc5de4ec697debd1853cb48b00a8e9f75efbf
Shiwani Mishra <mshiwani99@gmail.com>
2023-06-05 19:09:09 +0530

    refactor: move the position specific css to dashboard css and request and route component to page folder

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 import {Route, Routes} from "react-router-dom";
-import DashboardRoutes from "./component/DashboardRoutes";
-import DashboardRequests from "./component/DashboardRequests";
+import DashboardRoutes from "./pages/dashboard/Routes";
+import DashboardRequests from "./pages/dashboard/Requests";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} >
-          <Route index element={<DashboardRoutes />} />
+          <Route index element={<Routes />} />
           <Route path="requests" element={<DashboardRequests />} />
           <Route path="routes" element={<DashboardRoutes />} />
         </Route>

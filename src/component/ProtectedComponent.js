33acb942d694d3f4f8b8576e34ed2e9df3e77018
Shiwani Mishra <mshiwani99@gmail.com>
2023-06-05 20:09:37 +0530

    add: protected component

---
+++ src/component/ProtectedComponent.js
@@ -0,0 +1,7 @@
+import {withAuthenticationRequired} from "@auth0/auth0-react";
+
+const ProtectedComponent = ({ component, ...propsForComponent}) => {
+  const Component = withAuthenticationRequired(component);
+  return <Component {...propsForComponent} />;
+};
+export default ProtectedComponent;
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,14 +1,9 @@
-import {useAuth0} from "@auth0/auth0-react";
 import "./Dasboard.css";
 import PermanentDrawerLeft from "../../component/PermanentDrawerLeft";
 
 function Dashboard() {
-  const {user, isAuthenticated} = useAuth0();
   return (
-    isAuthenticated &&
-    user && (
-        <PermanentDrawerLeft />
-    )
+    <PermanentDrawerLeft />
   );
 }
 

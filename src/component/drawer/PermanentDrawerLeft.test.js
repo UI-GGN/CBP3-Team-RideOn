80d379f09e702e2d48d6f1a27bbd1e0719bc903d
Pulkit Kogta <pulkit.kogta@thoughtworks.com>
2023-06-06 07:38:20 +0530

    test: add snapshot test to permanentDrawerleft

--- src/component/drawer/PermanentDrawerLeft.test.js
+++ src/component/drawer/PermanentDrawerLeft.test.js
@@ -1,9 +1,9 @@
-import {render, screen, fireEvent, getByText, waitFor} from "@testing-library/react";
-import PermanentDrawerLeft from "./PermanentDrawerLeft";
+import { render, waitFor } from '@testing-library/react';
+import PermanentDrawerLeft from './PermanentDrawerLeft';
 
-describe("PermanentDrawerLeft", () => {
-  test("renders the component", async () => {
-    const {getByText, getByRole} = render(<PermanentDrawerLeft/>);
+describe('PermanentDrawerLeft', () => {
+  test('renders the component', async () => {
+    const { getByText, getByRole } = render(<PermanentDrawerLeft />);
 
     await waitFor(() => {
       expect(getByText('Routes')).toBeInTheDocument();
@@ -11,5 +11,11 @@ describe("PermanentDrawerLeft", () => {
       expect(getByText('Logout')).toBeInTheDocument();
       expect(getByRole('img')).toBeInTheDocument();
     });
+  });
+
+  test('should match snapshot', () => {
+    const { asFragment } = render(<PermanentDrawerLeft />);
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });

9cdbb7ac69da003b06276b67abfbe6c5f5d76c08
aishwaryaramesh1999 <r.aishwarya@thoughtworks.com>
2023-06-08 09:22:32 +0530

    fix: wrap common styles with themeprovider in main app class

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import "./App.css";
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 import {Route, Routes} from "react-router-dom";
+import {createTheme, ThemeProvider} from "@mui/material";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Nunito", "sans-serif"].join(","),
+    fontWeightMedium: "bold",
+  },
+});
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }

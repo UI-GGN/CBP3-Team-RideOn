ee75476b47c859186e81460bc5dfcbf3352aa106
Shiwani Mishra <mshiwani99@gmail.com>
2023-06-20 18:40:22 +0530

    feat: change avatar css

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -4,7 +4,7 @@ import {Avatar as MUIAvatar} from "@mui/material";
 export default function Avatar({imageLink}) {
   return (
     <MUIAvatar
-      sx={{width: 56, height: 56}}
+      sx={{width: 48, height: 48}}
       src={imageLink}
       imgProps={{referrerpolicy: "no-referrer"}}
     />

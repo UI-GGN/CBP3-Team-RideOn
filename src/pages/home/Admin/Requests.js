8462bf67c143703927a7a0ec056e5442d5a1efd0
Evantdm <evanjalin@tdmgrowth.com>
2023-07-17 16:43:02 +0530

    feat: Add date filter for export function [Evan]

--- src/pages/home/Admin/Requests.js
+++ src/pages/home/Admin/Requests.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AdminHome.css";
 import PaginatedTable from "../../../components/table/PaginatedTable";
 import {Box, Button} from "@mui/material";
@@ -8,8 +8,13 @@ import * as XLSX from "xlsx";
 import {saveAs} from "file-saver";
 import {rows} from "../../../constants";
 import {adminReqColumns, adminReqRow} from "../../../data";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function HomeRequests() {
+  const [fromDate, setFromDate] = useState();
+  const [tillDate, setTillDate] = useState();
+
   const convertJsonToWorkbook = (json) => {
     const worksheet = XLSX.utils.json_to_sheet(json);
     const workbook = XLSX.utils.book_new();
@@ -28,12 +33,33 @@ function HomeRequests() {
     <Box className="requestMain">
       <PaginatedTable columns={adminReqColumns} rows={adminReqRow} />
       <Box className="downloadContainer">
-        <span>Export Requests Report</span>
+        <div className="exportLabel" style={{ flex: "2" }}>Export Requests Report</div>
+        <div className="datepickerContainer" style={{ marginRight: "10px", flex: "1", display: "flex", alignItems: "center" }}>
+        <DatePicker
+              wrapperClassName="datePicker"
+              placeholderText="From Date"
+              selected={fromDate}
+              onChange={(date) => setFromDate(date)}
+              minDate={new Date()}
+              dateFormat="Pp"
+          />
+        </div>
+        <div className="datepickerContainer" style={{ flex: "1", display: "flex", alignItems: "center" }}>
+        <DatePicker
+              wrapperClassName="datePicker"
+              placeholderText="Till Date"
+              selected={tillDate}
+              onChange={(date) => setTillDate(date)}
+              minDate={new Date()}
+              dateFormat="Pp"
+          />
+        </div>
         <Button
-          className="downloadButton"
-          variant="contained"
-          size="medium"
-          onClick={handleDownload}
+            style={{ flex: "1", display: "flex", alignItems: "center"}}
+            className="downloadButton"
+            variant="contained"
+            size="medium"
+            onClick={handleDownload}
         >
           Download <GetAppIcon />
         </Button>

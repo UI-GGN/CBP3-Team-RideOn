276063796a8bae1db52b76ae5907341590baa9df
Shiwani Mishra <mshiwani99@gmail.com>
2023-08-06 17:03:47 +0530

    feat: Add bulk upload vendor

--- src/pages/home/Admin/Vendors.js
+++ src/pages/home/Admin/Vendors.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import "./AdminHome.css";
 import PaginatedTable from "../../../components/table/PaginatedTable";
-import {Box} from "@mui/material";
-import "./Request.css";
-import {vendorReqColumns} from "../../../tableHeader";
-import {useGetAllVendor} from "../../../services/Request/useGetAllVendor";
+import { Box, FormControl, Input, Button } from "@mui/material";
+import "./Vendors.css";
+import { vendorReqColumns } from "../../../tableHeader";
+import { useGetAllVendor } from "../../../services/Request/useGetAllVendor";
+import { useVendorBulkUpload } from "../../../services/Request/useVendorBulkUpload";
+import { APIStatus } from "../../../reducers/api-reducer";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GetAppIcon from "@mui/icons-material/GetApp";
 
 const getVendorRows = (requestList) => {
   return requestList?.data;
@@ -17,21 +22,76 @@ const columnVendorWidths = {
 
 const HomeVendors = () => {
   const [page, setPage] = useState(0);
-  const [params, setParams] = useState({"page-number": 1, limit: 10});
-  const {response: requestList, status} = useGetAllVendor(params);
+  const [params, setParams] = useState({ "page-number": 1, limit: 10 });
+  const { response: requestList, status } = useGetAllVendor(params);
   const vendorRowData = getVendorRows(requestList);
+  const [vendorExcelFile, setVendorExcelFile] = useState("");
+  const { bulkUploadVendor } = useVendorBulkUpload();
+  const [fileInputKey, setFileInputKey] = useState(0);
 
   const handleChangePage = async (_event, newPage) => {
     setPage(newPage);
-    setParams({"page-number": newPage + 1, limit: 10});
+    setParams({ "page-number": newPage + 1, limit: 10 });
+  };
+
+  const handleFileUpload = async (e) => {
+    setVendorExcelFile(e.target.files[0]);
+  };
+
+  const reset = () => {
+    setFileInputKey(Math.random().toString(36));
+    setVendorExcelFile("");
+  };
+
+  const handleOnSubmit = async () => {
+    const formData = new FormData();
+    formData.append("file", vendorExcelFile);
+    const status = await bulkUploadVendor(formData);
+    if (status === APIStatus.SUCCESS) {
+      showSuccessToastMessage();
+      setParams({ "page-number": 1, limit: 10 });
+    } else {
+      showErrorToastMessage();
+    }
+    reset();
+  };
+
+  const showSuccessToastMessage = () => {
+    toast.success("Successfully uploaded Vendors!");
+  };
+
+  const showErrorToastMessage = () => {
+    toast.error("Failed to upload the vendors!");
   };
 
   return (
-    <Box className="requestMain" style={{paddingBottom: 30}}>
+    <>
+      <Box className="requestMain" style={{ paddingBottom: 30 }}>
         <PaginatedTable columns={vendorReqColumns} rows={vendorRowData} page={page}
-        handleChangePage={handleChangePage} count={requestList?.metadata?.total} apiStatus={status}
-        width={columnVendorWidths} elevation={2}/>
-    </Box>
+          handleChangePage={handleChangePage} count={requestList?.metadata?.total} apiStatus={status}
+          width={columnVendorWidths} elevation={2} />
+        <Box className="uploadContainer">
+          <span>Bulk Upload Vendors</span>
+          <FormControl>
+            <Input
+              key={fileInputKey || ''}
+              type="file"
+              label="File"
+              id="my-input"
+              onClick={handleFileUpload} />
+          </FormControl>
+          <Button
+            className="uploadButton"
+            variant="contained"
+            size="medium"
+            onClick={handleOnSubmit}
+          >
+            Submit <GetAppIcon />
+          </Button>
+        </Box>
+      </Box>
+      <ToastContainer />
+    </>
   );
 };
 
